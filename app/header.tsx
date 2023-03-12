180b4aa25b895cfdbec87b6184581feaae93a984
Asil <asil9802mum@gmail.com>
2023-03-12 11:14:43 +0500

    mob view for hero implemented

--- app/header.tsx
+++ app/header.tsx
@@ -19,11 +19,9 @@ export function Header() {
     <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-white-800">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <a href="#" className="flex items-center">
-          <Image src={Logo} className="mr-3 h-6 sm:h-9" alt="Logo" />
+          <Image src={Logo} className="mr-3 h-6 w-full sm:h-9" alt="Logo" />
         </a>
         <div className="flex items-center lg:hidden">
-          {/* <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a> */}
-          {/* <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a> */}
           <button
             data-collapse-toggle="mobile-menu-2"
             type="button"
@@ -39,9 +37,9 @@ export function Header() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
             <svg
@@ -51,9 +49,9 @@ export function Header() {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
@@ -122,19 +120,19 @@ export function Header() {
         <div className="hidden lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
           <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
             <li className="flex justify-between items-center flex-row">
-              <Image src={Cloud} className="mr-3 h-6 sm:h-9" alt="" />
+              <Image src={Cloud} className="w-auto mr-3 h-6 sm:h-9" alt="" />
               <p className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 lg:p-0">
                 23 C°
               </p>
             </li>
             <li className="flex justify-between items-center flex-row">
-              <Image src={Clock} className="mr-3 h-6 sm:h-9" alt="" />
+              <Image src={Clock} className="w-auto mr-3 h-6 sm:h-9" alt="" />
               <p className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 lg:p-0">
                 12:45
               </p>
             </li>
             <li className="flex justify-between items-center flex-row">
-              <Image src={USD} className="mr-3 h-6 sm:h-9" alt="" />
+              <Image src={USD} className="w-auto mr-3 h-6 sm:h-9" alt="" />
               <p className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 lg:p-0">
                 11230
               </p>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,20 @@
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen bg-[url('../public/Hero.png')] bg-cover bg-center flex justify-center items-center">
+      <div className="w-full lg:h-screen h-[65vh] bg-[url('../public/Hero.png')] bg-cover bg-center flex justify-center items-center">
         <h1 className="font-bold flex flex-col items-center text-white">
-          <span className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] text-6xl">ИССЛЕДУЙТЕ</span>
-          <span className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] p-5 text-9xl">УЗБЕКИСТАН</span>
-          <span className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] text-6xl">ВСЕМЕ С НАМИ</span>
+          <span className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] lg:text-6xl text-3xl">ИССЛЕДУЙТЕ</span>
+          <span className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] p-5 lg:text-9xl text-6xl">УЗБЕКИСТАН</span>
+          <span className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] lg:text-6xl text-3xl">ВСЕМЕ С НАМИ</span>
           {/* change below basic yellow into custom color in config file */}
           <button className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.8)] bg-blue-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full mt-8">
             Бронировать
           </button>
         </h1>
       </div>
+      <>
+        just typing new content 
+      </>
     </>
   );
 }
