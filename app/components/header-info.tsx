03b3f369624f5755470c7a4465801786e93f8016
Asil <asil9802mum@gmail.com>
2023-03-23 01:37:06 +0500

    currency is now fetched form api

--- app/components/header-info.tsx
+++ app/components/header-info.tsx
@@ -5,37 +5,48 @@ import USD from "public/USD.svg";
 import { useEffect, useState } from "react";
 
 export default function HeaderInfo() {
- const [weather, setWeather] = useState<string>('');
- const currentDate = new Date();
-    
+  const [weather, setWeather] = useState<string>("");
+  const [currency, setCurrency] = useState<number>(11367);
+  const currentDate = new Date();
 
-    useEffect(() => {
-        fetch('https://www.meteosource.com/api/v1/free/point?lat=41.3N&lon=69.2E&sections=current%2Chourly&language=en&units=metric&key=y1f9tymubwge617ftelk5zjkw54dg3chf1ckup94')
-        .then(res => res.json())
-        .then(data => setWeather(data.current.temperature))
-        
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://www.meteosource.com/api/v1/free/point?lat=41.3N&lon=69.2E&sections=current%2Chourly&language=en&units=metric&key=y1f9tymubwge617ftelk5zjkw54dg3chf1ckup94"
+    )
+      .then((res) => res.json())
+      .then((data) => setWeather(data.current.temperature));
+  }, []);
 
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "mZB93UQkDfGcanNx5wrVgWbjzS5EyS7S");
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders
+  };
+  useEffect(() => {
+  
+  fetch("https://api.apilayer.com/exchangerates_data/convert?to=UZS&from=USD&amount=1", requestOptions)
+    .then(response => response.json())
+    .then(result => setCurrency(result.result))
+    .catch(error => console.log('error', error));
+  }, []);
 
   return (
     <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
       <li className="flex justify-between items-center flex-row">
         <Image src={Cloud} className="w-6 mr-3 h-6 sm:h-9" alt="" />
-        <p className="block text-sm text-darkBlue">
-          {weather} C°
-        </p>
+        <p className="block text-sm text-darkBlue">{weather} C°</p>
       </li>
       <li className="flex justify-between items-center flex-row">
         <Image src={Clock} className="w-6 mr-3 h-6 sm:h-9" alt="" />
         <p className="block text-sm text-darkBlue">
-          {currentDate.getHours() + ':' + currentDate.getMinutes()}
+          {currentDate.getHours() + ":" + currentDate.getMinutes()}
         </p>
       </li>
       <li className="flex justify-between items-center flex-row">
         <Image src={USD} className="w-au6-3 h-6 sm:h-9" alt="" />
-        <p className="block text-sm text-darkBlue">
-          11230
-        </p>
+        <p className="block text-sm text-darkBlue">{Math.round(currency)}</p>
       </li>
     </ul>
   );
