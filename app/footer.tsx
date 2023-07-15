d11e5a43dcfbb59f9b25eb0b8d696d44a36be459
Asil <asil9802mum@gmail.com>
2023-07-15 22:08:41 +0500

    fix: MR conflict 1 fix

--- app/footer.tsx
+++ app/footer.tsx
@@ -10,35 +10,45 @@ import { FaTripadvisor } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center">
-      <div className="container lg:px-[8%] lg:py-[4%] p-[4%] flex align-center flex-col lg:flex-row lg:space-x-12 drop-shadow-[0px_4px_10px_rgba(255,255,255,0.3)]">
-        <div className="flex flex-col grow-0 space-y-4">
-          <Image src={Logo} className="w-[100px] h-auto" alt="" />
-          <div className="text-sm">г. Самарканд, ул. Орзу Махмудова дом 18</div>
-          <div className="flex items-center gap-2 text-sm">
-            <BsFillTelephoneFill />
-            +998 99 771 73 30
-          </div>
-          <div className="flex items-center gap-2 text-sm">
-            <MdEmail />
-            info@afrasiab-travel.com
-          </div>
-          <div className="flex space-x-3 text-yellow">
-            <Link href="/">
-              <AiFillFacebook className="w-[25px] h-[25px]" />
-            </Link>
-            <Link href="/">
-              <AiFillInstagram className="w-[25px] h-[25px]" />
-            </Link>
-            <Link href="/">
-              <AiFillYoutube className="w-[25px] h-[25px]" />
-            </Link>
-            <Link href="/">
-              <FaTripadvisor className="w-[25px] h-[25px]" />
-            </Link>
-            <Link href="/">
-              <BsTelegram className="w-[25px] h-[25px]" />
-            </Link>
+    <div className="lg:px-[8%] lg:py-[4%] p-[4%] flex align-center flex-col lg:flex-row lg:space-x-12 drop-shadow-[0px_4px_10px_rgba(255,255,255,0.3)]">
+      <div className="flex flex-col grow-0 space-y-4">
+        <Image src={Logo} className="w-[100px] h-auto" alt="" />
+        <div className="text-sm">г. Самарканд, ул. Орзу Махмудова дом 18</div>
+        <div className="flex items-center gap-2 text-sm">
+          <BsFillTelephoneFill />
+          +998 99 771 73 30
+        </div>
+        <div className="flex items-center gap-2 text-sm">
+          <MdEmail />
+          info@afrasiab-travel.com
+        </div>
+        <div className="flex space-x-3 text-yellow">
+          <Link href="https://www.facebook.com/Afrasiabtravel?mibextid=LQQJ4d">
+            <AiFillFacebook className="w-[25px] h-[25px]" />
+          </Link>
+          <Link href="https://www.instagram.com/turi_po_uzbekistanu/">
+            <AiFillInstagram className="w-[25px] h-[25px]" />
+          </Link>
+          {/* <Link href="/">
+            <AiFillYoutube className="w-[25px] h-[25px]" />
+          </Link> */}
+          <Link href="/">
+            <FaTripadvisor className="w-[25px] h-[25px]" />
+          </Link>
+          <Link href="https://t.me/afrasiabtravell">
+            <BsTelegram className="w-[25px] h-[25px]" />
+          </Link>
+        </div>
+      </div>
+      <div className="grow lg:flex justify-between">
+        <div className="flex flex-col">
+          <h4 className="text-base font-bold">Главная</h4>
+          <div className="mt-2 flex lg:flex-col sm:flex-row lg:space-y-2 justify-between">
+            <Link href="/">Главная</Link>
+            <Link href="/about">О нас</Link>
+            <Link href="/tours">Авторские Туры</Link>
+            <Link href="/cities">Города</Link>
+            <Link href="/contacts">Контакты</Link>
           </div>
         </div>
         <div className="grow lg:flex justify-between">
