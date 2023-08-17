6f6c3240d88e39c3f794b44ff266c8b9f12b0344
Asilbek Muminov <asilbekmuminov@Asilbeks-MacBook-Air.local>
2023-08-18 02:06:56 +0500

    booking almost done

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+"use client";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { svgs } from "../components/FormUIsvgs";
 import { tours } from "../data/data";
 import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import TourCard from "../components/TourCard";
 
 type Inputs = {
   name: string;
@@ -19,6 +22,14 @@ export default function BookingPage() {
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const [selectedTour, setSelectedTour] = useState<any>(null);
+
+  const handleTourChange = (title: string) => {
+    console.log(tours, title);
+    setSelectedTour(
+      tours.find((tour) => `${tour.title} ${tour.price}` === title)
+    );
+  };
 
   return (
     <div className="lg:px-[8%] lg:py-[4%] lg:text-start px-[4%] py-[2%] text-center ">
@@ -97,21 +108,32 @@ export default function BookingPage() {
                         </p>
                       </div>
                     </div> */}
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Выберете тур
-                </label>
-                <select
-                  id="coutourntries"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                >
-                  <option selected>Выберете Тур</option>
-                  {tours.map((tour: any) => (
-                    <option>
-                      {tour.title}
-                      {tour.price}
-                    </option>
-                  ))}
-                </select>
+                <div className="flex flex-col p-2 gap-4">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Выберете тур
+                  </label>
+                  <select
+                    value={selectedTour ?? "Выберете Тур"}
+                    onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                      console.log(event.target);
+                      handleTourChange(event.target.value);
+                    }}
+                    id="coutourntries"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[320px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    {/* <option selected>Выберете Тур</option> */}
+                    {tours.map((tour: any) => (
+                      <option>{`${tour.title} ${tour.price}`}</option>
+                    ))}
+                  </select>
+                  {selectedTour && (
+                    <TourCard
+                      {...(tours.find(
+                        (tour) => tour.title === selectedTour.title
+                      ) as any)}
+                    />
+                  )}
+                </div>
               </div>
               <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
                 <div className="relative rounded-lg bg-white lg:p-8 shadow-lg sm:p-12">
@@ -170,12 +192,32 @@ export default function BookingPage() {
                         </div>
                       )}
                     </div>
+
+                    <div className="relative max-w-sm">
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                        <svg
+                          className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                        </svg>
+                      </div>
+                      <input
+                        type="date"
+                        className="text-body-color border-[#112B3C] focus:border-primary w-full resize-none rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                        placeholder="Select date"
+                      />
+                    </div>
+
                     <div className="mb-6">
                       <textarea
-                        rows={6}
+                        rows={3}
                         defaultValue=""
                         {...register("message")}
-                        placeholder="Ваше сообщение"
+                        placeholder="Ваш комментарий к брони"
                         className="text-body-color border-[#112B3C] focus:border-primary w-full resize-none rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
                       ></textarea>
                     </div>
